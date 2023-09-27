09cb79b907a4b8afaca836a2265a6624712202b0
Vlad Glazunov <vlad.glazunov.02@gmail.com>
2023-09-27 18:06:54 +0300

    init spring project

---
+++ src/main/java/com/justtown/image_to_webp/ImageToWebpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.justtown.image_to_webp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageToWebpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageToWebpServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justtown/image_to_webp/ImageToWebpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justtown.image_to_webp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageToWebpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
